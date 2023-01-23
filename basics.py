597d58c96c893136b20deac8074e420e7d8e574c
tufailzaman <tufailzaman789@gamil.com>
2023-01-22 21:06:33 -0800

    List

---
+++ basics.py
@@ -0,0 +1,39 @@
+# # puthon data Types
+ 
+# a = [1,8,3,2,5,3,5,9,0]
+# print(a[2])
+# age = 36
+# #text = "My name is Khan And i am " + age
+# #print (text.format(age))
+# print(len(a))
+# print(type(a))
+# a[2] = "Tufail"
+# print(a)
+# a.insert(1,"Zaman")
+# a.insert(0,"Tufail Zaman")
+# print(a)
+# a.append("Khan")
+# print(a)
+
+list = ["Tufail","Awais","Amjad","Anas","Habib"]
+list1 = [1,2,3,4,5,6]
+print(len(list))
+print(list[3])
+list[1] = "Html" 
+list.insert(0,"python")
+list.append("JavaScript")
+print(list)
+list.extend(list1)
+print(list)
+list.append(list1)
+print(list)
+list.remove("Amjad")
+print(list)
+list.pop(0)
+print(list)
+#list.clear()
+#print(list)
+list.reverse()
+print(list)
+
+#Tuple

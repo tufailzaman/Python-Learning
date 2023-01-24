87440c9c47780482b412499eef34cfee83a5964e
tufailzaman <tufailzaman789@gamil.com>
2023-01-23 23:27:26 -0800

    Variable

--- basics.py
+++ basics.py
@@ -1,19 +1,19 @@
-# # puthon data Types
+# python data Types
  
-# a = [1,8,3,2,5,3,5,9,0]
-# print(a[2])
-# age = 36
-# #text = "My name is Khan And i am " + age
-# #print (text.format(age))
-# print(len(a))
-# print(type(a))
-# a[2] = "Tufail"
-# print(a)
-# a.insert(1,"Zaman")
-# a.insert(0,"Tufail Zaman")
-# print(a)
-# a.append("Khan")
-# print(a)
+a = [1,8,3,2,5,3,5,9,0]
+print(a[2])
+age = 36
+#text = "My name is Khan And i am " + age
+#print (text.format(age))
+print(len(a))
+print(type(a))
+a[2] = "Tufail"
+print(a)
+a.insert(1,"Zaman")
+a.insert(0,"Tufail Zaman")
+print(a)
+a.append("Khan")
+print(a)
 
 list = ["Tufail","Awais","Amjad","Anas","Habib"]
 list1 = [1,2,3,4,5,6]
@@ -30,10 +30,55 @@ print(list)
 list.remove("Amjad")
 print(list)
 list.pop(0)
+print(list) 
+list.reverse()
 print(list)
 #list.clear()
 #print(list)
-list.reverse()
-print(list)
 
-#Tuple
+### Python Indentation  == we will use in program otherwise  python will gave error
+### it refers to spaces in the begening
+
+if 5 > 2:
+    print("Five is greater than two")
+
+#### Variables
+#Assign Multiple values
+
+x,y,z = "orange","Apple","Banana"
+print(x)
+print(y)
+print(z)
+
+x=y=z = "Mango"
+print(x)
+print(y)
+print(z)
+
+## Unpack a Collection
+
+fruits =[ "apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+##Output Variables
+
+x = "Python is Awesome"
+print (x) ## Here "x" is output variable because used is print() function
+
+x="python"
+y="is"
+z="Awesome"
+print (x,y,z)
+
+x = 5
+y = 8
+print(x+y)
+
+a = "Tufail Zamna"
+b=5
+c="2"
+print(a,b)
+print(a+c)
